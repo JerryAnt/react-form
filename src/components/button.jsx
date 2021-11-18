e5df98e163197ce5bb81e106e2cf771dd7567765
Jeremy Antoine <jeremincanada@gmail.com>
2021-11-18 17:33:34 -0500

    added testin to study validations and make my own

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,6 +2,7 @@ import React,  { Component } from 'react';
 
 class Button extends Component {
   render() {
+
     return (
       <div className="buttons input-field" id={this.props.id}>{this.props.id}</div>
     );
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,21 +2,27 @@ import React,  { Component } from 'react';
 import Button from './button.jsx';
 import Checkbox from './checkbox.jsx';
 import InputField from './input_field.jsx';
-class Form extends Component {
 
+class Form extends Component {
   render() {
     return (
       <div>
-        <div><InputField  id="name" /></div>
-
-        <div><InputField  id="company" /></div>
-
-        <div><InputField  id="email" /></div>
-
+        <div>
+          <p>name*</p>
+          <InputField  id="name" />
+        </div>
+        <div>
+          <p>company*</p>
+          <InputField  id="company" />
+        </div>
+        <div>
+          <p>email*</p>
+          <InputField  id="email" />
+        </div>
         <Checkbox />
         <div class="container">
-          <Button id="reset" />
           <Button type="submit" id="submit" />
+          <Button id="reset" />
         </div>
       </div>
     );
