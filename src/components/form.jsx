4914698fce00e95180c3ced33c81a09f67063b3c
Jeremy Antoine <jeremincanada@gmail.com>
2021-11-19 19:18:00 -0500

    pseudo code on handleSubmit and handleClear

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -7,16 +7,43 @@ class Form extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      // where is should put my events
+      // where I should put my events
     };
   }
 
 handleSubmit() {
   // where I should change the state
   // handleSubmit for the submit button
+
+    // when I click on submit button
+      // if input field is empty
+      // show an error message
+
+      // if email is empty
+      // show an error message
+
+      // if company is empty
+        // show an error message
+
+      // if email is not valid
+      // show an error message
+
+  // else submit button throws to a link
+
 }
 
 
+handleClear() {
+  // where I should change the state
+  // handleClear for the reset button
+
+    // when I click on reset button
+      // input field is empty   ""
+      // email field is empty ""
+      // company field is empty ""
+      // checkbox: check false
+
+}
 
   render() {
     return (
